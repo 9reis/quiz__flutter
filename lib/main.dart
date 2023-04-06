cd38ca7c9bc4bd33270223e2c57673b6d22ed65b
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-06 18:39:11 -0300

    Lista para Widgets #2

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
         'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
       }
     ];
-
-    List<Widget> respostas = [];
-
-    for (String textoResp
-        in perguntas[_perguntaSelecionada].cast()['respostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
+    List<String> respostas =
+        perguntas[_perguntaSelecionada].cast()['respostas'];
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -47,7 +42,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: [
             Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...respostas
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
           ],
         ),
       ),

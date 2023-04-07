92366a183c346cf057d887a43e62311dde39a2be
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-07 07:55:35 -0300

    Reiniciando Questionario

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder,
               )
-            : Resultado(_pontuacaoTotal),
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
@@ -72,7 +72,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
         _pontuacaoTotal += pontuacao;
       });
     }
-    print(_pontuacaoTotal);
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPerguntaSelecionada {
--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Resultado extends StatelessWidget {
-  const Resultado(this.pontuacao);
+  const Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
 
   final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
 
   String get fraseResultado {
     if (pontuacao < 8) {
@@ -19,13 +21,26 @@ class Resultado extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          fraseResultado,
-          style: TextStyle(fontSize: 28),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+          ),
         ),
-      ),
+        TextButton(
+          child: Text(
+            'Reiniciar ?',
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 16,
+            ),
+          ),
+          onPressed: quandoReiniciarQuestionario,
+        ),
+      ],
     );
   }
 }

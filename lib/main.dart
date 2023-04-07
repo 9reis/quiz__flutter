5c0dd9cc8188740365a6a44665dc0d6dacd01ef0
Lucas Reis <lucasreis_9@hotmail.com>
2023-04-06 21:31:22 -0300

    Exibindo Resultado

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas.map((t) => Resposta(t, _responder)).toList(),
                 ],
               )
-            : null,
+            : Center(
+                child: Text(
+                  'Parabéns',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
